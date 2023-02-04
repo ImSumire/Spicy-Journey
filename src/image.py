8460fc76d764e5d72e3649bc750dcb9e02fa8790
Galaad <75391703+ImGalaad@users.noreply.github.com>
2023-02-04 14:51:02 +0100

    Adding `.convert_alpha()`

--- src/image.py
+++ src/image.py
@@ -11,13 +11,8 @@ class Image:
 
     def __init__(self, file):
         self.file = file
-        self.image = pygame.image.load(file)
+        self.image = pygame.image.load(file).convert_alpha()
         self.size = self.image.get_size()
-        self.x_mod = randint(0, 6)
-        self.y_mod = randint(0, 4)
-        # print(self.size)
 
     def display(self, surf, position):
-        # self.x_mod = randint(0,6)
-        # self.y_mod = randint(0,4)
-        surf.blit(self.image, (position[0] + self.x_mod, position[1] + self.y_mod))
+        surf.blit(self.image, (position[0], position[1]))

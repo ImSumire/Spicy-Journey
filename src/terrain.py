35a4fbbf0546f82159c4b6cc06efb5c31b7095f9
Galaad <75391703+ImGalaad@users.noreply.github.com>
2023-03-04 13:13:02 +0100

    Update terrain.py

--- src/terrain.py
+++ src/terrain.py
@@ -1,6 +1,11 @@
 # Imports
 import pygame
-import noise as ns
+from noise import snoise2
+from math import fmod
+from random import randint
+
+from src.player import Player
+
 from src.memoize import memoize
 
 
@@ -12,19 +17,17 @@ def isometric_perspective(x, y, x_offset, y_offset):
 class Terrain:
     def __init__(
         self,
-        block: pygame.image,
-        vegetation: pygame.image,
         frequence_height: float,
         width: int,
         height: int,
     ):
-        self.block = block
-        self.vegetation = vegetation
+
+        self.block = pygame.image.load("res/sprites/Terrain/grass.png").convert_alpha()
 
         self.frequence_height = frequence_height
 
         self.x_center = width // 4
-        self.y_center = height // 4 - 5
+        self.y_center = height // 4
 
         self.render_distance = 45
         self.center = self.render_distance // 2
@@ -32,11 +35,52 @@ class Terrain:
         self.x_offset = self.x_center - self.render_distance
         self.y_offset = self.y_center - self.render_distance
 
-        self.width_offset = self.block.size[0] // 2
-        self.height_offset = self.block.size[1] // 2
+        self.width_offset = self.block.get_width() // 2
+        self.height_offset = self.block.get_height() // 2
+
+        self.vegetation = []
+        for image in (
+            ["little-tree1"]
+            + ["little-tree2"]
+            + ["tree1"]
+            + ["tree2"]
+            + ["tree3"]
+            + ["rock" + str(n) for n in range(1, 6)]
+            # bigrock0 | bigrock1 | bigrock2 | brush
+        ):
+            self.vegetation.append(
+                pygame.image.load(
+                    "res/sprites/Props/{}.png".format(image)
+                ).convert_alpha()
+            )
+
+        self.vegetation_shadows = []
+        for image in (
+            ["little-tree1"]
+            + ["little-tree2"]
+            + ["tree1"]
+            + ["tree2"]
+            + ["tree3"]
+            + ["rock" + str(n) for n in range(1, 6)]
+            # bigrock0 | bigrock1 | bigrock2 | brush
+        ):
+            self.vegetation_shadows.append(
+                pygame.image.load(
+                    "res/sprites/Props/shadow/{}.png".format(image)
+                ).convert_alpha()
+            )
+
+        self.coords = []
+        self.sprites_list = []
+
+        self.seed = randint(-10e4, 10e4)
+
+        self.spawn = (0, 0)
+
+        self.temp = 93
 
     @memoize
-    def _process_coord(self, x, y, camera_pos):
+    def _process_coord(self, x, y, player_x, player_y):
         x_pos, y_pos = isometric_perspective(
             x,
             y,
@@ -46,54 +90,122 @@ class Terrain:
             - ((self.render_distance - 0.5) // 2) * self.height_offset,
         )
 
-        y_pos = y_pos + 10 * ns.snoise2(
-            (x + int(camera_pos[0])) * self.frequence_height,
-            (y + int(camera_pos[1])) * self.frequence_height,
+        y_pos = y_pos + 10 * snoise2(
+            (x + player_x) * self.frequence_height,
+            (y + player_y) * self.frequence_height,
+            base=self.seed,
         )
 
-        props_noise = ns.snoise2(
-            (x + int(camera_pos[0])) * 0.03,
-            (y + int(camera_pos[1])) * 0.03,
+        props_noise = snoise2(
+            (x + player_x) * 0.03, (y + player_y) * 0.03, base=self.seed
         )
         return x_pos, y_pos, props_noise
 
-    @memoize
-    def get_coords(self, camera_pos):
-        coords_cache = []
+    def get_coords(self, player_x, player_y):
+        self.coords = []
 
-        for y in range(0, self.render_distance):
+        for y in range(self.render_distance):
             coords_cache_sub = []
-            for x in range(0, self.render_distance):
-                coords_cache_sub.append(self._process_coord(x, y, camera_pos))
-            coords_cache.append(coords_cache_sub)
+            for x in range(self.render_distance):
+                coords_cache_sub.append(
+                    self._process_coord(x, y, int(player_x), int(player_y))
+                )
+            self.coords.append(coords_cache_sub)
 
-        return coords_cache
+    def get_sprites(self, player):
+        x_offset = int((fmod(player.pos.x, 1) - fmod(player.pos.y, 1)) * 16)
+        y_offset = int((fmod(player.pos.x, 1) + fmod(player.pos.y, 1)) * 8)
 
-    def draw_terrain(self, surf, camera_pos):
-        self.coords = self.get_coords(camera_pos)
-        for row in self.coords:
-            for column in row:
+        player_pos = self.coords[self.center][self.center]
+
+        self.terrain_sprites = []
+        # self.props_shadows = []
+        self.props_sprites = []
+
+        player_blited = False
+
+        for y, row in enumerate(self.coords):
+            for x, column in enumerate(row):
+
                 x_pos, y_pos, props_noise = column
-                surf.blit(
-                    self.block.image,
-                    (
-                        x_pos,
-                        y_pos,
-                    ),
+
+                self.terrain_sprites.append(
+                    (self.block, x_pos - x_offset, y_pos - y_offset)
                 )
 
-    def draw_props(self, surf, camera_pos):
-        self.coords = self.get_coords(camera_pos)
-        for row in self.coords:
-            for column in row:
-                x_pos, y_pos, props_noise = column
                 if round(props_noise):
                     prop = self.vegetation[int(str(props_noise)[-1])]
-
-                    surf.blit(
-                        prop.image,
+                    prop_shadow = self.vegetation_shadows[int(str(props_noise)[-1])]
+                    #prop = self.vegetation[2]
+                    self.props_sprites.append(
                         (
-                            x_pos,
-                            y_pos - prop.size[1] + 15,
-                        ),
+                            prop,
+                            x_pos - x_offset,
+                            y_pos - y_offset - prop.get_height() + 15,
+                        )
                     )
+                    # self.props_shadows.append(
+                    #     (
+                    #         prop_shadow,
+                    #         x_pos - x_offset,
+                    #         y_pos - y_offset - prop.get_height() + 15,
+                    #     )
+                    # )
+
+                if not player_blited:
+                    if (
+                        x - 1 > self.render_distance // 2
+                        and y - 1 > self.render_distance // 2
+                    ):
+                        self.props_sprites.append(
+                            (player.image, player_pos[0], player_pos[1] + 16)
+                        )
+                        player_blited = True
+
+        #print(len(self.coords))
+
+        # self.props_sprites = sorted(self.props_sprites, key=lambda sprite: (sprite[2], sprite[1]))
+        # print(self.terrain_sprites)
+        # print(type(player))
+        # for sprite in self.props_sprites:
+        #     print('P' if isinstance(sprite[0], Player) else 'G', round(sprite[1]), round(sprite[2]), end=' | ')
+        # exit()
+        # self.terrain_sprites += self.props_shadows
+        return self.terrain_sprites, self.props_sprites
+
+    def get_sprites2(self, player):
+        x_offset = int((fmod(player.pos.x, 1) - fmod(player.pos.y, 1)) * 16)
+        y_offset = int((fmod(player.pos.x, 1) + fmod(player.pos.y, 1)) * 8)
+
+        player_pos = self.coords[self.center][self.center]
+
+        self.terrain_sprites = []
+        self.props_sprites = []
+
+        for y, row in enumerate(self.coords):
+            for x, column in enumerate(row):
+
+                x_pos, y_pos, props_noise = column
+
+                self.terrain_sprites.append(
+                    (self.block, x_pos - x_offset, y_pos - y_offset)
+                )
+
+                if round(props_noise):
+                    prop = self.vegetation[int(str(props_noise)[-1])]
+                    prop_shadow = self.vegetation_shadows[int(str(props_noise)[-1])]
+                    self.props_sprites.append(
+                        (
+                            prop,
+                            x_pos - x_offset,
+                            y_pos - y_offset - prop.get_height() + 15,
+                        )
+                    )
+                else:
+                    self.props_sprites.append(())
+
+        self.props_sprites.insert(
+                                  len(self.props_sprites) // 2 + self.temp,
+                                  (player.image, player_pos[0], player_pos[1] + 16)
+                                 )
+        return self.terrain_sprites, [_ for _ in self.props_sprites if _]

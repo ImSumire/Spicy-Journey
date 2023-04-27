705953d3c0e6c1fcf1627babbbbbd2650868535b
Sumire <75391703+ImSumire@users.noreply.github.com>
2023-04-27 09:50:38 +0200

    Update lang.py

--- src/tools/lang.py
+++ src/tools/lang.py
@@ -1,6 +1,16 @@
+"""
+Ce module comprend une fonction qui a pour but de lire les fichiers .lang
+Les fichiers de données .lang sont inspirés de ce dans le jeu Minecraft.
+"""
+
+# pylint: disable=consider-using-f-string
+
 import os
 
 def get_langs():
+    """
+    Retourne la liste de tous les noms des fichiers .lang
+    """
     return [
         lang.replace(".lang", "") for lang in os.listdir("res/lang")
     ]
@@ -12,10 +22,10 @@ def load_lang(file):
     ~ 0,00000197/lignes
     """
 
-    with open("res/lang/%s.lang" % file, 'r') as f:
-        lines = f.readlines()
+    with open("res/lang/%s.lang" % file, 'r', encoding="utf-8") as file:
+        lines = file.readlines()
 
-    items = {}
+    words = {}
     for line in lines:
         # Ignorer les commentaires
         if line.startswith('##') or not line:
@@ -26,13 +36,12 @@ def load_lang(file):
         if index != -1:
             key = line[:index]
             valeur = line[index+1:].strip()
-            items[key] = valeur
+            words[key] = valeur
 
-    return items
+    return words
 
 if __name__ == '__main__':
-    items = load_lang('en.lang')
+    words = load_lang('en.lang')
 
-    print(items['accessibility.disableTTS']) # affiche 'Text To Speech disabled'
-    print(items['accessibility.enableTTS']) # affiche 'Text To Speech enabled'
-
+    print(words['accessibility.disableTTS']) # affiche 'Text To Speech disabled'
+    print(words['accessibility.enableTTS']) # affiche 'Text To Speech enabled'

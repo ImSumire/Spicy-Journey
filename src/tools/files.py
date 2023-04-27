222d9584312c3eddb5ceeb91096d92f52022c9ae
Sumire <75391703+ImSumire@users.noreply.github.com>
2023-04-27 09:46:13 +0200

    Update files.py

--- src/tools/files.py
+++ src/tools/files.py
@@ -1,20 +1,24 @@
-# La fonction  prend en entrée un  chemin  d'accès à un répertoire contenant des
-# images. Elle parcourt récursivement le répertoire en utilisant  "os.walk" pour
-# récupérer  la liste de tous les fichiers  d'images,  et charge  chaque image à
-# l'aide de "pygame.image.load".
-# 
-# Les images sont stockées dans une liste et  renvoyées à la fin de la fonction.
-# La  méthode "convert_alpha" est  utilisée pour  optimiser les  performances de
-# l'affichage des images dans Pygame  en préparant les images  à une utilisation
-# avec la transparence alpha.
+"""
+La fonction  prend en entrée un  chemin  d'accès à un répertoire contenant des
+images. Elle parcourt récursivement le répertoire en utilisant  "os.walk" pour
+récupérer  la liste de tous les fichiers  d'images,  et charge  chaque image à
+l'aide de "pygame.image.load".
 
+Les images sont stockées dans une liste et  renvoyées à la fin de la fonction.
+La  méthode "convert_alpha" est  utilisée pour  optimiser les  performances de
+l'affichage des images dans Pygame  en préparant les images  à une utilisation
+avec la transparence alpha.
+"""
 
-from src.tools.memoize import memoize
-import pygame
 import os
+import pygame
+from src.tools.memoize import memoize
 
 @memoize
 def get_images(path):
+    """
+    Récupère toutes les images d'un fichier et les converties avec pygame.
+    """
     surface_list = []
     for _, __, img_files in os.walk(path):
         for image in img_files:
